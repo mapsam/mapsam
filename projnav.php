e885fb8157d95085c324115721c3a8bb80b248f2
Sam Matthews <matthews.sam@gmail.com>
2013-04-08 19:00:05 -0700

    Create projnav.php
    Navigation bar for projects. My first custom php 'include' !

---
+++ projnav.php
@@ -0,0 +1,28 @@
+<div id="nav">
+  <div id="title"><a href="index.php">mapsam</a></div>
+	<ul>
+		<li><a href="about.php" class="bold">about</a></li>
+		<li><a href="images/matthews-resume.pdf" class="bold">resume</a></li>
+	</ul>
+	<ul>
+		<li><a href="p_nacis.php">nacis booklet map</a></li>
+		<li><a href="p_mtns.php">mountains and rivers</a></li>
+		<li><a href="p_renewable.php">renewable energy map</a></li>
+		<li><a href="p_wirec.php">wisconsin recreation series</a></li>
+		<li><a href="p_ltc.php">land tenure publication</a></li>
+		<li><a href="p_oqi.php">office of quality improvement</a></li>
+		<li><a href="p_axis.php">seattle typographic</a></li>
+		<li><a href="p_roadtrip.php">seattle roadtrip (interactive)</a></li>
+		<li><a href="p_native.php">historical america series</a></li>
+		<li><a href="p_edge.php">edge project branding</a></li>
+		<li><a href="p_pope.php">pope farm park map & signs</a></li>
+		<li><a href="p_bologna.php">bologna process map</a></li>
+		<li><a href="p_lcsb.php">string band flier</a></li>
+	</ul>
+	<div id="iconContainerProj">
+		<a href="https://github.com/svmatthews" target="_blank"><div class="icon" id="first"></div></a>
+		<a href="https://twitter.com/vancematthews" target="_blank"><div class="icon" id="second"></div></a>
+		<a href="http://www.linkedin.com/in/svmatthews" target="_blank"><div class="icon" id="third"></div></a>
+		<a href="http://www.facebook.com/matthews.sam" target="_blank" target="_blank"><div class="icon" id="fourth"></div></a>
+	</div>
+</div>
