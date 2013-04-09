8f3255008281d25b5ecd4b3327dafe4eff285c4e
Sam Matthews <matthews.sam@gmail.com>
2013-04-08 18:56:51 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <title>mapsam</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css'>
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.js"></script>
+
+</head>
+
+<body>
+	<div id="main">
+		<div id="image" class="shift" ></div>
+		<div id="content">
+			<h1>mapsam</h1>
+			<p>I’m a Seattle based designer working on
+websites, cartographic productions, and 
+other digital curiosities. I'm the lead designer & co-founder of <a href="http://situatedlaboratories.com" class="sl">Situated 
+Laboratories</a> and I contract with <a href="http://goo.gl/maps/oswWR" class="goog">Google Maps</a>.
+
+<p>I take care of a <a href="http://giscollective.org" class="gisc">collaborative blog</a> and am also an assistant editor of a <a href="http://atlasofdesign.org" class="aod">design atlas</a>. This is my <a href="projects.php" class="proj">portfolio</a> and space for <a href="mugs.php" class="exp">experiments</a>.<p>
+		</div>
+		<div id="iconContainer">
+			<a href="https://github.com/svmatthews" target="_blank"><div class="icon" id="first"></div></a>
+			<a href="https://twitter.com/vancematthews" target="_blank"><div class="icon" id="second"></div></a>
+			<a href="http://www.linkedin.com/in/svmatthews" target="_blank"><div class="icon" id="third"></div></a>
+			<a href="http://www.facebook.com/matthews.sam" target="_blank" target="_blank"><div class="icon" id="fourth"></div></a>
+		</div>
+	</div>
+</body>
